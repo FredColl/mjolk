9ac29c9919f3a276a9a6cc53d0588e59a06a81e0
Fredrik Colldahl Scherstén <3694166+frsc@users.noreply.github.com>
2024-07-19 23:07:08 +0200

    feat: init

---
+++ src/app/login/page.tsx
@@ -0,0 +1,41 @@
+import { login, signup } from "./actions";
+
+export default function LoginPage() {
+  return (
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+        <label className="text-md" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="email"
+          placeholder="you@example.com"
+          required
+        />
+        <label className="text-md" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="password"
+          name="password"
+          placeholder="••••••••"
+          required
+        />
+        <button
+          formAction={login}
+          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+        >
+          Sign In
+        </button>
+        <button
+          formAction={signup}
+          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}

5f556a02850e022b3582dbb4904b28d5a7d22be8
Fredrik Colldahl Scherstén <3694166+frsc@users.noreply.github.com>
2024-07-20 19:43:16 +0200

    fix: nav in bottom

--- src/app/private/navigation.tsx
+++ src/app/private/navigation.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import {
   ChartBarSquareIcon,
   PencilSquareIcon,
   DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 // ...
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   const links = [
     {
       name: "Report",
@@ -25,6 +31,7 @@ export default function NavLinks() {
       icon: DocumentDuplicateIcon,
     },
   ];
+
   return (
     <>
       <div className="flex fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
@@ -36,7 +43,12 @@ export default function NavLinks() {
                 key={link.name}
                 href={link.href}
                 style={{ minWidth: "150px" }}
-                className="text-black flex grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+                className={clsx(
+                  "flex grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                  {
+                    "bg-sky-100 text-blue-600": pathname === link.href,
+                  }
+                )}
               >
                 <LinkIcon className="w-6" />
                 <p className="hidden md:block text-black">{link.name}</p>

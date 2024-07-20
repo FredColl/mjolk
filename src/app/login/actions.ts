a7b16f61aae01d5fa1627500cd7c0a35a03bba3d
Fredrik Colldahl Scherstén <3694166+frsc@users.noreply.github.com>
2024-07-20 10:31:35 +0200

    fix: redirect after login

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -21,7 +21,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath("/", "layout");
-  redirect("/yay");
+  redirect("/private/reportMilk");
 }
 
 export async function signup(formData: FormData) {

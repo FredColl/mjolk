f13c1b992a94b27a82b957dec615950b748af21e
Fredrik Colldahl Scherstén <3694166+frsc@users.noreply.github.com>
2024-07-20 15:26:56 +0200

    fix: nav in bottom

--- src/app/private/navigation.tsx
+++ src/app/private/navigation.tsx
@@ -27,15 +27,16 @@ export default function NavLinks() {
   ];
   return (
     <>
-      <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-        <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+      <div className="flex fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+        <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
           {links.map((link) => {
             const LinkIcon = link.icon;
             return (
               <Link
                 key={link.name}
                 href={link.href}
-                className="text-black flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+                style={{ minWidth: "150px" }}
+                className="text-black flex grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
               >
                 <LinkIcon className="w-6" />
                 <p className="hidden md:block text-black">{link.name}</p>

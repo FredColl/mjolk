2934b19d0581058faf174c04b8c34e5fb4d3cfc0
Fredrik Colldahl Scherstén <3694166+frsc@users.noreply.github.com>
2024-07-23 22:44:44 +0200

    fix: styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,41 +1,66 @@
+"use client";
+import { useState } from "react";
 import { login, signup } from "./actions";
+import { Button, Input, Spacer } from "@nextui-org/react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+    await login(formData);
+  };
+
+  const handleSignup = async (e: any) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+    await signup(formData);
+  };
+
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button
-          formAction={login}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-        >
-          Sign In
-        </button>
-        <button
-          formAction={signup}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-        >
-          Sign Up
-        </button>
-      </form>
+    <div className="flex min-h-screen justify-center items-center p-4">
+      <div className="flex flex-col w-full max-w-md p-6  rounded-lg ">
+        <h3 className="mb-6 text-center">Welcome Back</h3>
+        <form className="flex flex-col w-full gap-4" onSubmit={handleLogin}>
+          <label htmlFor="email">Email</label>
+          <Input
+            fullWidth
+            size="lg"
+            name="email"
+            placeholder="you@example.com"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Spacer y={1} />
+          <label htmlFor="password">Password</label>
+          <Input
+            type="password"
+            fullWidth
+            size="lg"
+            name="password"
+            placeholder="••••••••"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Spacer y={1.5} />
+          <Button type="submit" color="primary" size="lg" className="mb-2">
+            Sign In
+          </Button>
+        </form>
+        <form className="flex flex-col w-full gap-4" onSubmit={handleSignup}>
+          <Button type="submit" color="primary" size="lg">
+            Sign Up
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,62 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { Button, Card, CardBody, CardHeader, Spacer } from "@nextui-org/react";
 
 export default function Home() {
   const router = useRouter();
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="flex flex-col items-center justify-center mr-8 ml-8">
-        <div className="bg-indigo-600 rounded-full p-6 mb-4">
-          {/* Simple logo */}
-          <svg
-            className="h-16 w-16 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 8c1.657 0 3 1.343 3 3v1c0 1.657-1.343 3-3 3s-3-1.343-3-3v-1c0-1.657 1.343-3 3-3z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 14v6m0 0c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zM12 14a4 4 0 100-8 4 4 0 000 8z"
-            />
-          </svg>
-        </div>
-        <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center">
-          Dr Jiang Xintongs Milk Tracker
-        </h1>
-        <p className="text-lg text-gray-600 mb-6 text-center">
-          Track your breastfeeding and milk supplement with ease
-        </p>
-
-        <div className="flex space-x-4">
-          <button
-            onClick={() => router.push("/login")}
-            className="py-2 px-4 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            Sign Up
-          </button>
-          <button
-            onClick={() => router.push("/login")}
-            className="py-2 px-4 bg-green-600 text-white font-semibold rounded-md shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
-          >
-            Login
-          </button>
-        </div>
-      </div>
+      <Card className="flex flex-col items-center justify-center p-8 ">
+        <CardHeader className="flex justify-center">
+          <div className="bg-indigo-600 rounded-full p-6 mb-4">
+            <svg
+              className="h-16 w-16 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 8c1.657 0 3 1.343 3 3v1c0 1.657-1.343 3-3 3s-3-1.343-3-3v-1c0-1.657 1.343-3 3-3z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 14v6m0 0c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zM12 14a4 4 0 100-8 4 4 0 000 8z"
+              />
+            </svg>
+          </div>
+        </CardHeader>
+        <CardBody className="flex flex-col items-center text-center">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">
+            Dr Jiang Xintongs Milk Tracker
+          </h1>
+          <div className="text-lg text-gray-600 mb-6">
+            Track your breastfeeding and milk supplement with ease
+          </div>
+          <Spacer y={1} />
+          <div className="flex space-x-4">
+            <Button
+              onClick={() => router.push("/signup")}
+              color="primary"
+              className="shadow-md mr-2"
+            >
+              Sign Up
+            </Button>
+            <Button
+              onClick={() => router.push("/login")}
+              color="success"
+              className="shadow-md"
+            >
+              Login
+            </Button>
+          </div>
+        </CardBody>
+      </Card>
     </div>
   );
 }

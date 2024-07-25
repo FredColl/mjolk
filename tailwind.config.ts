8d28fece52f3c093b81a97732ae05dba6705a781
Fredrik Colldahl Scherstén <3694166+frsc@users.noreply.github.com>
2024-07-25 18:30:40 +0200

    chore: config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,9 @@ import { nextui } from "@nextui-org/react";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
